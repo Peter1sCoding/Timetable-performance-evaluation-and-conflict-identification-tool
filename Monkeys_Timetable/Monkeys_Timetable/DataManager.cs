c50d236e030fdba6cd2014b29eeed18d684df86d
16251187 <16251187@bjtu.edu.cn>
2019-11-26 11:03:58 +0800

    为DataManager类创建了列车和车站列表

--- Monkeys_Timetable/Monkeys_Timetable/DataManager.cs
+++ Monkeys_Timetable/Monkeys_Timetable/DataManager.cs
@@ -7,6 +7,29 @@ namespace Monkeys_Timetable
 {
     class DataManager  //读入到发时刻及车站等数据的类，封装读取列车、车站等方法
     {
-        
+        private List<Train> x_trainList; //列车列表
+        public List<Train> trainList
+        {
+            get 
+            {
+                return x_trainList; 
+            }
+            set
+            {
+                value = x_trainList;
+            }
+        }
+        private List<string> x_stationList;//车站列表
+        public List<string> stationList
+        {
+            get
+            {
+                return x_stationList;
+            }
+            set
+            {
+                value = x_stationList;
+            }
+        }
     }
 }
