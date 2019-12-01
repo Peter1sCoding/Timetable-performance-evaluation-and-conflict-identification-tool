e31d33a8338ab7704b36892f205c497f0189deb0
恺 徐 <1697776578@qq.com>
2019-12-01 15:27:38 +0800

    assessment

--- Monkeys_Timetable/Monkeys_Timetable/Assessment.cs
+++ Monkeys_Timetable/Monkeys_Timetable/Assessment.cs
@@ -7,6 +7,27 @@ namespace Monkeys_Timetable
 {
     class Assessment//封装各类运行图指标计算方法
     {
-        ////
+        public DataManager aDataManager = new DataManager();
+        public double GetTravelSpeed()
+        {
+            double SumMile = 0;
+            foreach (Train aTrain in aDataManager.trainList)
+            {
+                SumMile += Convert.ToDouble(aTrain.trainNo);//添加方法后改为SumMile += aTrain.GetMile();
+            }
+            double travelSpeed = SumMile / aDataManager.trainList.Count();
+            return travelSpeed;
+        }
+
+        public double GetTechnicalSpeed()
+        {
+            double SumMile = 0;
+            foreach (Train aTrain in aDataManager.trainList)
+            {
+                SumMile += Convert.ToDouble(aTrain.trainNo);//添加方法后改为SumMile += aTrain.GetMile();
+            }
+            double travelSpeed = SumMile / aDataManager.trainList.Count();
+            return travelSpeed;
+        }
     }
 }
