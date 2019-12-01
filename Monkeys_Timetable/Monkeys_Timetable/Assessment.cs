788e838f256149408ce74e9c353347511a4b5734
恺 徐 <1697776578@qq.com>
2019-12-01 16:43:22 +0800

    assessment

--- Monkeys_Timetable/Monkeys_Timetable/Assessment.cs
+++ Monkeys_Timetable/Monkeys_Timetable/Assessment.cs
@@ -15,19 +15,64 @@ namespace Monkeys_Timetable
             {
                 SumMile += Convert.ToDouble(aTrain.trainNo);//添加方法后改为SumMile += aTrain.GetMile();
             }
-            double travelSpeed = SumMile / aDataManager.trainList.Count();
-            return travelSpeed;
+            double TravelSpeed = SumMile / aDataManager.trainList.Count();//添加方法后改为 RunningTime += trainlaststation.ArriveTime -  trainfirststation.DeptureTime;
+            return TravelSpeed;
         }
 
         public double GetTechnicalSpeed()
         {
             double SumMile = 0;
+            double RunningTime = 0;
             foreach (Train aTrain in aDataManager.trainList)
             {
                 SumMile += Convert.ToDouble(aTrain.trainNo);//添加方法后改为SumMile += aTrain.GetMile();
             }
-            double travelSpeed = SumMile / aDataManager.trainList.Count();
-            return travelSpeed;
+            foreach (Train aTrain in aDataManager.trainList)
+            {
+                RunningTime += Convert.ToDouble(aTrain.trainNo);//添加方法后改为 RunningTime += trainNo.ArriveTime - trainNo+1.DeptureTime;(for i从开始到最后)
+            }
+            double TechnicalSpeed = SumMile / RunningTime;
+            return TechnicalSpeed;
+        }
+
+        public double GetIndexOfSpeed()
+        {
+            double IndexOfSpeed = GetTravelSpeed() / GetTechnicalSpeed();
+            return IndexOfSpeed;
+        }
+
+        public int GetServiceFrequency()
+        {
+            int ServiceFrequency = 0;
+            //输入数字得到需要查看哪一辆车的服务频率
+            //if(trainNo.DeptureTime or trainNo.ArriveTime == null)
+            //{
+            //    ServiceFrequency += 1;
+            //}
+            //elseif(trainNo.DeptureTime - trainNo.ArriveTime > 0)
+            //{
+            //    ServiceFrequency += 1;
+            //}
+            return ServiceFrequency;
+        }
+
+        public int GetStationServiceCount()
+        {
+            //输入数字得到需要查看哪一个站的服务次数
+            int StationServiceCount = 0;
+            foreach (Train aTrain in aDataManager.trainList)
+            {
+                //每一辆车在这个站的出发时间-到达时间>0 
+                //if(trainNo.DeptureTime or trainNo.ArriveTime == null)
+                //{
+                //    ServiceFrequency += 1;
+                //}
+                //elseif(trainNo.DeptureTime - trainNo.ArriveTime > 0)
+                //{
+                //    ServiceFrequency += 1;
+                //}
+            }
+            return StationServiceCount;
         }
     }
 }
