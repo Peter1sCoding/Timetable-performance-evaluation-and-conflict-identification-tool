fae510299630f52542e4bfffcba06bb539487d4a
lisiqiao_bjtu@163.com <lisiqiao_bjtu@163.com>
2019-12-11 12:05:03 +0800

    panel2布置

--- Monkeys_Timetable/Monkeys_Timetable/AssessForm.cs
+++ Monkeys_Timetable/Monkeys_Timetable/AssessForm.cs
@@ -17,17 +17,16 @@ namespace Monkeys_Timetable
         {
             InitializeComponent();
             ShowFirst();
-            
         }
         
         public void ShowFirst()
         {
-            this.Height = 800;
+            this.Height = 700;
             this.Width = 1200;
             this.splitContainer1.Dock = DockStyle.Fill;
             splitContainer1.SplitterDistance = 499;
             splitContainer1.SplitterWidth = 2;
-            splitContainer2.SplitterDistance = 399;
+            splitContainer2.SplitterDistance = 349;
             splitContainer2.SplitterWidth = 2;
             splitContainer1.IsSplitterFixed = true;
             splitContainer2.IsSplitterFixed = true;
@@ -38,78 +37,81 @@ namespace Monkeys_Timetable
         {
             Label lbStation = new Label();
             lbStation.Text = "选择车站";
-            lbStation.Size = new Size(100, 50);
-            lbStation.Location = new Point(100, 450);
+            lbStation.Font = new Font("宋体", 10, FontStyle.Bold);
+            lbStation.Size = new Size(100, 30);
+            lbStation.Location = new Point(100, 50);
             this.splitContainer2.Panel2.Controls.Add(lbStation);
 
+
             ComboBox cbStation = new ComboBox();
+            //cbStation.Items.AddRange(new object[] { 1, 2, 3 });
             foreach (Train tra in aDataManager.TrainList)
             {
                 string trainName = tra.trainNo;
                 cbStation.Items.Add(trainName);
             }
-            cbStation.Size = new Size(150, 50);
-            cbStation.Location = new Point(250, 450);
+            cbStation.Size = new Size(100, 30);
+            cbStation.Location = new Point(200, 50);
             splitContainer2.Panel2.Controls.Add(cbStation);
 
             Label lbTime1 = new Label();
             lbTime1.Text = "6:00-9:00";
-            lbTime1.Size = new Size(150, 50);
-            lbTime1.Location = new Point(10, 400 + 120);
+            lbTime1.Size = new Size(80, 30);
+            lbTime1.Location = new Point(10, 90);
             splitContainer2.Panel2.Controls.Add(lbTime1);
             TextBox tbTime1 = new TextBox();
-            tbTime1.Size = new Size(150, 50);
-            tbTime1.Location = new Point(10, 400 + 120 + 50 + 5);
+            tbTime1.Size = new Size(80, 50);
+            tbTime1.Location = new Point(10, 90 + 30);
             splitContainer2.Panel2.Controls.Add(tbTime1);
 
             Label lbTime2 = new Label();
             lbTime2.Text = "9:00-12:00";
-            lbTime2.Size = new Size(150, 50);
-            lbTime2.Location = new Point(10 + 150 + 10, 400 + 120);
+            lbTime2.Size = new Size(80, 30);
+            lbTime2.Location = new Point(10 + 150 + 10, 90);
             splitContainer2.Panel2.Controls.Add(lbTime2);
             TextBox tbTime2 = new TextBox();
-            tbTime2.Size = new Size(150, 50);
-            tbTime2.Location = new Point(10 + 150 + 10, 400 + 120 + 50 + 5);
+            tbTime2.Size = new Size(80, 50);
+            tbTime2.Location = new Point(10 + 150 + 10, 90 + 30);
             splitContainer2.Panel2.Controls.Add(tbTime2);
 
             Label lbTime3 = new Label();
             lbTime3.Text = "12:00-15:00";
-            lbTime3.Size = new Size(150, 50);
-            lbTime3.Location = new Point(10 + 150 * 2 + 10 * 2, 400 + 120);
+            lbTime3.Size = new Size(80, 30);
+            lbTime3.Location = new Point(10 + 150 * 2 + 10 * 2, 90);
             splitContainer2.Panel2.Controls.Add(lbTime3);
             TextBox tbTime3 = new TextBox();
-            tbTime3.Size = new Size(150, 50);
-            tbTime3.Location = new Point(10 + 150 * 2 + 10 * 2, 400 + 120 + 50 + 5);
+            tbTime3.Size = new Size(80, 50);
+            tbTime3.Location = new Point(10 + 150 * 2 + 10 * 2, 90 + 30);
             splitContainer2.Panel2.Controls.Add(tbTime3);
 
             Label lbTime4 = new Label();
             lbTime4.Text = "15:00-18:00";
-            lbTime4.Size = new Size(150, 50);
-            lbTime4.Location = new Point(10, 400 + 120 + 50 * 2 + 5 + 20);
+            lbTime4.Size = new Size(80, 30);
+            lbTime4.Location = new Point(10, 90 + 30 + 50 + 5);
             splitContainer2.Panel2.Controls.Add(lbTime4);
             TextBox tbTime4 = new TextBox();
-            tbTime4.Size = new Size(150, 50);
-            tbTime4.Location = new Point(10, 400 + 120 + 50 * 3 + 5 * 2 + 20);
+            tbTime4.Size = new Size(80, 50);
+            tbTime4.Location = new Point(10, 90 + 30 * 2 + 50 + 5);
             splitContainer2.Panel2.Controls.Add(tbTime4);
 
             Label lbTime5 = new Label();
             lbTime5.Text = "18:00-21:00";
-            lbTime5.Size = new Size(150, 50);
-            lbTime5.Location = new Point(10 + 150 + 10, 400 + 120 + 50 * 2 + 5 + 20);
+            lbTime5.Size = new Size(80, 30);
+            lbTime5.Location = new Point(10 + 150 + 10, 90 + 30 + 50 + 5);
             splitContainer2.Panel2.Controls.Add(lbTime5);
             TextBox tbTime5 = new TextBox();
-            tbTime5.Size = new Size(150, 50);
-            tbTime5.Location = new Point(10 + 150 + 10, 400 + 120 + 50 * 3 + 5 * 2 + 20);
+            tbTime5.Size = new Size(80, 50);
+            tbTime5.Location = new Point(10 + 150 + 10, 90 + 30 * 2 + 50 + 5);
             splitContainer2.Panel2.Controls.Add(tbTime5);
 
             Label lbTime6 = new Label();
             lbTime6.Text = "21:00-24:00";
-            lbTime6.Size = new Size(150, 50);
-            lbTime6.Location = new Point(10 + 150 * 2 + 10 * 2, 400 + 120 + 50 * 2 + 5 + 20);
+            lbTime6.Size = new Size(80, 30);
+            lbTime6.Location = new Point(10 + 150 * 2 + 10 * 2, 90 + 30 + 50 + 5);
             splitContainer2.Panel2.Controls.Add(lbTime6);
             TextBox tbTime6 = new TextBox();
-            tbTime6.Size = new Size(150, 50);
-            tbTime6.Location = new Point(10 + 150 * 2 + 10 * 2, 400 + 120 + 50 * 3 + 5 * 2 + 20);
+            tbTime6.Size = new Size(80, 50);
+            tbTime6.Location = new Point(10 + 150 * 2 + 10 * 2, 90 + 30 * 2 + 50 + 5);
             splitContainer2.Panel2.Controls.Add(tbTime6);
         }
 
