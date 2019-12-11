595ae4b254cd75b640af928e7d9fcd0e086e3650
lisiqiao_bjtu@163.com <lisiqiao_bjtu@163.com>
2019-12-11 08:53:35 +0800

    assessForm初始化

--- Monkeys_Timetable/Monkeys_Timetable/AssessForm.cs
+++ Monkeys_Timetable/Monkeys_Timetable/AssessForm.cs
@@ -14,7 +14,24 @@ namespace Monkeys_Timetable
         public AssessForm()
         {
             InitializeComponent();
+            ShowFirst();
         }
 
+        public void ShowFirst()
+        {
+            this.Height = 800;
+            this.Width = 1200;
+            this.splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.IsSplitterFixed = true;
+            splitContainer2.IsSplitterFixed = true;
+            this.splitContainer1.Panel1.Width = 500;
+            splitContainer2.Panel1.Height = splitContainer2.Height / 2;
+        }
+
+        public void ShowServiceFrequency()
+        {
+            
+
+        }
     }
 }
