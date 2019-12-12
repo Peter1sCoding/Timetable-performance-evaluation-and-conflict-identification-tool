2beb9795da22649547d1917c7f3567d79ef9138c
lisiqiao_bjtu@163.com <lisiqiao_bjtu@163.com>
2019-12-12 10:34:57 +0800

    panel2控件之间的数据传递

--- Monkeys_Timetable/Monkeys_Timetable/AssessForm.cs
+++ Monkeys_Timetable/Monkeys_Timetable/AssessForm.cs
@@ -13,6 +13,8 @@ namespace Monkeys_Timetable
     {
         DataManager aDataManager = new DataManager();
         Assessment ass = new Assessment();
+        Dictionary<string, int[]> serviceCount = new Dictionary<string, int[]>();
+        
         public AssessForm()
         {
             InitializeComponent();
@@ -30,12 +32,31 @@ namespace Monkeys_Timetable
             splitContainer2.SplitterWidth = 2;
             splitContainer1.IsSplitterFixed = true;
             splitContainer2.IsSplitterFixed = true;
-            ShowPanel2();
+            ShowPanel_2();
         }
 
-        public void ShowPanel2()
+        #region Panel_2 全局变量
+        Label lbStation;
+        ComboBox cbStation;
+        Label lbTime1;
+        TextBox tbTime1;
+        Label lbTime2;
+        TextBox tbTime2;
+        Label lbTime3;
+        TextBox tbTime3;
+        Label lbTime4;
+        TextBox tbTime4;
+        Label lbTime5;
+        TextBox tbTime5;
+        Label lbTime6;
+        TextBox tbTime6;
+        Button btRun;
+        #endregion
+
+        public void ShowPanel_2()
         {
-            Label lbStation = new Label();
+            lbStation = new Label();
+            lbStation.Name = "lbStation";
             lbStation.Text = "选择车站";
             lbStation.Font = new Font("宋体", 10, FontStyle.Bold);
             lbStation.Size = new Size(80, 30);
@@ -43,7 +64,8 @@ namespace Monkeys_Timetable
             this.splitContainer2.Panel2.Controls.Add(lbStation);
 
 
-            ComboBox cbStation = new ComboBox();
+            cbStation = new ComboBox();
+            cbStation.Name = "cbStation";
             //cbStation.Items.AddRange(new object[] { 1, 2, 3 });
             foreach (Train tra in aDataManager.TrainList)
             {
@@ -54,81 +76,108 @@ namespace Monkeys_Timetable
             cbStation.Location = new Point(210, 50);
             splitContainer2.Panel2.Controls.Add(cbStation);
 
-            Label lbTime1 = new Label();
+
+            lbTime1 = new Label();
             lbTime1.Text = "6:00-9:00";
             lbTime1.Size = new Size(80, 20);
             lbTime1.Location = new Point(50, 100);
             splitContainer2.Panel2.Controls.Add(lbTime1);
-            TextBox tbTime1 = new TextBox();
+            tbTime1 = new TextBox();
+            tbTime1.Name = "tbTime1";
             tbTime1.Size = new Size(80, 50);
             tbTime1.Location = new Point(50, 100 + 20);
             splitContainer2.Panel2.Controls.Add(tbTime1);
 
-            Label lbTime2 = new Label();
+            lbTime2 = new Label();
             lbTime2.Text = "9:00-12:00";
             lbTime2.Size = new Size(80, 20);
             lbTime2.Location = new Point(50 + 150 + 10, 100);
             splitContainer2.Panel2.Controls.Add(lbTime2);
-            TextBox tbTime2 = new TextBox();
+            tbTime2 = new TextBox();
+            tbTime2.Name = "tbTime2";
             tbTime2.Size = new Size(80, 50);
             tbTime2.Location = new Point(50 + 150 + 10, 100 + 20);
             splitContainer2.Panel2.Controls.Add(tbTime2);
 
-            Label lbTime3 = new Label();
+            lbTime3 = new Label();
             lbTime3.Text = "12:00-15:00";
             lbTime3.Size = new Size(80, 20);
             lbTime3.Location = new Point(50 + 150 * 2 + 10 * 2, 100);
             splitContainer2.Panel2.Controls.Add(lbTime3);
-            TextBox tbTime3 = new TextBox();
+            tbTime3 = new TextBox();
+            tbTime3.Name = "tbTime3";
             tbTime3.Size = new Size(80, 50);
             tbTime3.Location = new Point(50 + 150 * 2 + 10 * 2, 100 + 20);
             splitContainer2.Panel2.Controls.Add(tbTime3);
 
-            Label lbTime4 = new Label();
+            lbTime4 = new Label();
             lbTime4.Text = "15:00-18:00";
             lbTime4.Size = new Size(80, 20);
             lbTime4.Location = new Point(50, 100 + 30 + 50 + 5);
             splitContainer2.Panel2.Controls.Add(lbTime4);
-            TextBox tbTime4 = new TextBox();
+            tbTime4 = new TextBox();
+            tbTime4.Name = "tbTime4";
             tbTime4.Size = new Size(80, 50);
             tbTime4.Location = new Point(50, 100 + 30 * 2 + 50);
             splitContainer2.Panel2.Controls.Add(tbTime4);
 
-            Label lbTime5 = new Label();
+            lbTime5 = new Label();
             lbTime5.Text = "18:00-21:00";
             lbTime5.Size = new Size(80, 20);
             lbTime5.Location = new Point(50 + 150 + 10, 100 + 30 + 50 + 5);
             splitContainer2.Panel2.Controls.Add(lbTime5);
-            TextBox tbTime5 = new TextBox();
+            tbTime5 = new TextBox();
+            tbTime5.Name = "tbTime5";
             tbTime5.Size = new Size(80, 50);
             tbTime5.Location = new Point(50 + 150 + 10, 100 + 30 * 2 + 50);
             splitContainer2.Panel2.Controls.Add(tbTime5);
 
-            Label lbTime6 = new Label();
+            lbTime6 = new Label();
             lbTime6.Text = "21:00-24:00";
             lbTime6.Size = new Size(80, 20);
             lbTime6.Location = new Point(50 + 150 * 2 + 10 * 2, 100 + 30 + 50 + 5);
             splitContainer2.Panel2.Controls.Add(lbTime6);
-            TextBox tbTime6 = new TextBox();
+            tbTime6 = new TextBox();
+            tbTime6.Name = "tbTime6";
             tbTime6.Size = new Size(80, 50);
             tbTime6.Location = new Point(50 + 150 * 2 + 10 * 2, 100 + 30 * 2 + 50);
             splitContainer2.Panel2.Controls.Add(tbTime6);
 
-            Button btRun = new Button();
+            btRun = new Button();
             btRun.Text = "查询车站服务次数";
             btRun.Font = new Font("宋体", 10, FontStyle.Bold);
             btRun.Size = new Size(150, 40);
             btRun.Location = new Point(180, 100 + 30 * 2 + 50 * 2);
             this.splitContainer2.Panel2.Controls.Add(btRun);
+            btRun.Click += BtRun_Click;
         }
 
-        public void ShowServiceFrequency()
+        private void BtRun_Click(object sender, EventArgs e)
         {
-            Dictionary<string, int[]> serviceCount = new Dictionary<string, int[]>();
-            serviceCount = ass.GetStationServiceCount();
-
-
-            
+            //throw new NotImplementedException();
+            if (cbStation.SelectedItem == null)
+            {
+                MessageBox.Show("请先选择车站！");
+            }
+            else
+            {
+                String str = cbStation.SelectedItem.ToString();
+                for (int i = 1; i < 7; i++)
+                {
+                    string str1 = "lbTime" + i.ToString();
+                    string str2 = "tbTime" + i.ToString();
+                    foreach (Control control in this.splitContainer2.Panel2.Controls)
+                    {
+                        if (control is TextBox && control.Name == str2)
+                        {
+                            (control as TextBox).Text = str + "+" + i.ToString();
+                            break;
+                        }
+                    }
+                }
+            }
         }
+
     }
+    
 }
