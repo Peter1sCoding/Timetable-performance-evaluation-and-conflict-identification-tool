aad8f58fa58ad381ac6621f82c4125a3e1a4a42d
lisiqiao_bjtu@163.com <lisiqiao_bjtu@163.com>
2019-12-19 12:06:31 +0800

    终点站

--- Monkeys_Timetable/Monkeys_Timetable/AssessForm.cs
+++ Monkeys_Timetable/Monkeys_Timetable/AssessForm.cs
@@ -370,15 +370,15 @@ namespace Monkeys_Timetable
 
         private void 查询列车停站信息ToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            DataManager dmm = dm;
-           for(int i = 0; i < dmm.TrainList.Count; i++)
+           for(int i = 0; i < dm.TrainList.Count; i++)
            {
                 string strsta = "";
-                if (dmm.TrainList[i].trainNo == cbTrain.SelectedItem)
+                
+                if (dm.TrainList[i].trainNo == cbTrain.SelectedItem.ToString())
                 {
-                    for (int j = 0; j < dmm.TrainList[70].staList.Count - 1; j++)
+                    for (int j = 0; j < dm.TrainList[i].staList.Count; j++)
                     {
-                        strsta = strsta + "\n" + dmm.TrainList[70].staList[j];
+                        strsta = strsta + "\n" + dm.TrainList[i].staList[j];
                     }
                     MessageBox.Show(strsta);
                     break;
