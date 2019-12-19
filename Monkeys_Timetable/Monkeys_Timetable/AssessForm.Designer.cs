f5b77c64ffa01db50006246801ae88fbcf33e4c6
lisiqiao_bjtu@163.com <lisiqiao_bjtu@163.com>
2019-12-19 12:26:58 +0800

    ~

--- Monkeys_Timetable/Monkeys_Timetable/AssessForm.Designer.cs
+++ Monkeys_Timetable/Monkeys_Timetable/AssessForm.Designer.cs
@@ -30,9 +30,9 @@
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.绘制区间列车密度图ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.查询列车停站信息ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.splitContainer2 = new System.Windows.Forms.SplitContainer();
-            this.查询列车停站信息ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
@@ -49,57 +49,62 @@
             this.查询列车停站信息ToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1047, 28);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(7, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(1178, 32);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // 绘制区间列车密度图ToolStripMenuItem
             // 
             this.绘制区间列车密度图ToolStripMenuItem.Name = "绘制区间列车密度图ToolStripMenuItem";
-            this.绘制区间列车密度图ToolStripMenuItem.Size = new System.Drawing.Size(158, 24);
+            this.绘制区间列车密度图ToolStripMenuItem.Size = new System.Drawing.Size(184, 28);
             this.绘制区间列车密度图ToolStripMenuItem.Text = "绘制区间列车密度图";
             this.绘制区间列车密度图ToolStripMenuItem.Click += new System.EventHandler(this.绘制区间列车密度图ToolStripMenuItem_Click);
+            // 
+            // 查询列车停站信息ToolStripMenuItem
+            // 
+            this.查询列车停站信息ToolStripMenuItem.Name = "查询列车停站信息ToolStripMenuItem";
+            this.查询列车停站信息ToolStripMenuItem.Size = new System.Drawing.Size(166, 28);
+            this.查询列车停站信息ToolStripMenuItem.Text = "查询列车停站信息";
+            this.查询列车停站信息ToolStripMenuItem.Click += new System.EventHandler(this.查询列车停站信息ToolStripMenuItem_Click);
             // 
             // splitContainer1
             // 
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer1.Location = new System.Drawing.Point(0, 28);
+            this.splitContainer1.Location = new System.Drawing.Point(0, 32);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.splitContainer1.Name = "splitContainer1";
             // 
             // splitContainer1.Panel1
             // 
             this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
-            this.splitContainer1.Size = new System.Drawing.Size(1047, 592);
-            this.splitContainer1.SplitterDistance = 385;
+            this.splitContainer1.Size = new System.Drawing.Size(1178, 712);
+            this.splitContainer1.SplitterDistance = 433;
             this.splitContainer1.TabIndex = 1;
             // 
             // splitContainer2
             // 
             this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.splitContainer2.Name = "splitContainer2";
             this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
-            this.splitContainer2.Size = new System.Drawing.Size(385, 592);
-            this.splitContainer2.SplitterDistance = 236;
+            this.splitContainer2.Size = new System.Drawing.Size(433, 712);
+            this.splitContainer2.SplitterDistance = 283;
+            this.splitContainer2.SplitterWidth = 5;
             this.splitContainer2.TabIndex = 0;
-            // 
-            // 查询列车停站信息ToolStripMenuItem
-            // 
-            this.查询列车停站信息ToolStripMenuItem.Name = "查询列车停站信息ToolStripMenuItem";
-            this.查询列车停站信息ToolStripMenuItem.Size = new System.Drawing.Size(143, 24);
-            this.查询列车停站信息ToolStripMenuItem.Text = "查询列车停站信息";
-            this.查询列车停站信息ToolStripMenuItem.Click += new System.EventHandler(this.查询列车停站信息ToolStripMenuItem_Click);
             // 
             // AssessForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1047, 620);
+            this.ClientSize = new System.Drawing.Size(1178, 744);
             this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "AssessForm";
-            this.Text = "AssessForm";
+            this.Text = "运行图指标统计";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.splitContainer1.Panel1.ResumeLayout(false);
