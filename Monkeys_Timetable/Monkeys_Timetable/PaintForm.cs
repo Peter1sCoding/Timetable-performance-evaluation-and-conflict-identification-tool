db27eeef8aa50520a86f1e4cb12cfa64c3256f4d
余怡然 <ASUS@W519L>
2019-12-30 16:54:22 +0800

    1111

--- Monkeys_Timetable/Monkeys_Timetable/PaintForm.cs
+++ Monkeys_Timetable/Monkeys_Timetable/PaintForm.cs
@@ -50,8 +50,6 @@ namespace Monkeys_Timetable
             ci = new Conflict_Identification(dm.stationList, dm.HeadwayDic, dm.TrainDic);
             ci.Conflict_Judge();
             dt = ci.ToDataTable();
-
-
         }
 
         private void PaintForm_Load(object sender, EventArgs e)
