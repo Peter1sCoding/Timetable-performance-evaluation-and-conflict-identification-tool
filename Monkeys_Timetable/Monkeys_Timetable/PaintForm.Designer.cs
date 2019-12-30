ba3c3ae4da885f27bcc69b37feaf16f7d0f630a4
余怡然 <ASUS@W519L>
2019-12-30 14:32:52 +0800

    1111

--- Monkeys_Timetable/Monkeys_Timetable/PaintForm.Designer.cs
+++ Monkeys_Timetable/Monkeys_Timetable/PaintForm.Designer.cs
@@ -348,22 +348,24 @@
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
             this.button1.Location = new System.Drawing.Point(333, 0);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(40, 23);
             this.button1.TabIndex = 13;
             this.button1.Text = "放大";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
             this.button2.Location = new System.Drawing.Point(379, 0);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(40, 23);
             this.button2.TabIndex = 14;
             this.button2.Text = "缩小";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // PaintForm
